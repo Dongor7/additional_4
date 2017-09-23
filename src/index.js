131241d96f14fef1ee18a38b9c050e7200ff3db2
Denis Sidorenko <dongor7@gmail.com>
2017-09-23 15:49:01 +0300

    not so simple as I thought

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-module.exports = function multiply(first, second) {
-  return first * second;
+module.exports = function multiply(a, b) {
+    if (parseInt(a) == 0 || parseInt(b) == 0) {
+        return '0';
+    }
+
+    a = a.split('').reverse();
+    b = b.split('').reverse();
+    var result = [];
+
+    for (var i = 0; a[i] >= 0; i++) {
+        for (var j = 0; b[j] >= 0; j++) {
+            if (!result[i + j]) {
+                result[i + j] = 0;
+            }
+
+            result[i + j] += a[i] * b[j];
+        }
+    }
+
+    for (var i = 0; result[i] >= 0; i++) {
+        if (result[i] >= 10) {
+            if (!result[i + 1]) {
+                result[i + 1] = 0;
+            }
+
+            result[i + 1] += parseInt(result[i] / 10);
+            result[i] %= 10;
+        }
+    }
+
+    return result.reverse().join('');
 }
